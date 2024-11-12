5a2af99c6d31005bfc09e78780066c1664fd945d
r-keenan <ross.keenan@protonmail.com>
2024-11-12 14:09:04 -0600

    Updated readme to provide disclaimer. Updated control center to be searching for the proper endpoint.

--- Books.AppHost/Program.cs
+++ Books.AppHost/Program.cs
@@ -13,21 +13,31 @@ var kafka = builder
     .WithEnvironment("KAFKA_NODE_ID", "1")
     .WithEnvironment("KAFKA_PROCESS_ROLES", "broker,controller")
     .WithEnvironment("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@0.0.0.0:9093")
-    .WithEnvironment("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093")
-    .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://kafka:9092")
+    .WithEnvironment(
+        "KAFKA_LISTENERS",
+        "BROKER://0.0.0.0:29092,CONTROLLER://0.0.0.0:9093,EXTERNAL://0.0.0.0:9092"
+    )
+    .WithEnvironment(
+        "KAFKA_ADVERTISED_LISTENERS",
+        "BROKER://localhost:29092,EXTERNAL://localhost:9092"
+    )
     .WithEnvironment(
         "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP",
-        "PLAINTEXT:PLAINTEXT,CONTROLLER:PLAINTEXT"
+        "CONTROLLER:PLAINTEXT,BROKER:PLAINTEXT,EXTERNAL:PLAINTEXT"
     )
     .WithEnvironment("KAFKA_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
-    .WithEnvironment("KAFKA_INTER_BROKER_LISTENER_NAME", "PLAINTEXT")
+    .WithEnvironment("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER")
     .WithEnvironment("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
     .WithEnvironment("KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS", "0")
     .WithEnvironment("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1")
     .WithEnvironment("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
-    .WithEndpoint(9092, 9092, name: "broker")
+    .WithEnvironment("KAFKA_LOG_DIRS", "/var/lib/kafka/data")
+    .WithEndpoint(9092, 9092, name: "external")
     .WithEndpoint(9093, 9093, name: "controller")
-    .WithTopics(KafkaTopics.GetAllTopics(), numPartitions: 3, replicationFactor: 1);
+    .WithEndpoint(29092, 29092, name: "broker")
+    .WithTopics(KafkaTopics.GetAllTopics(), numPartitions: 3, replicationFactor: 1)
+    .WithVolume("kafka_data", "/var/lib/kafka/data")
+    .WithVolume("kafka_metadata", "/var/lib/kafka/metadata");
 
 var controlCenter = builder
     .AddContainer("control-center", "docker.io/confluentinc/cp-enterprise-control-center")
@@ -39,7 +49,7 @@ var controlCenter = builder
     .WithEnvironment("CONFLUENT_CONTROLCENTER_INTERNAL_TOPICS_REPLICATION", "1")
     .WithEnvironment("PORT", "9021")
     .WithEndpoint(9021, 9021, name: "control-center")
-    .WithReference(kafka.GetEndpoint("broker")); // Expose UI port
+    .WithReference(kafka.GetEndpoint("external")); // Expose UI port
 
 builder.AddProject<Projects.Books_API>("books-api").WithReference(kafka.GetEndpoint("broker"));
 
--- README.md
+++ README.md
@@ -10,6 +10,12 @@
 - Apache Kafka
 - Confluent Cloud Control Center
 
+## Disclaimer
+
+In a real-world application, we would probably only use RabbitMQ or Kafka, not both. Both are being used for demonstration purposes only. Right now this is a single API, but it will be split up into microservices in future iterations.
+
+Also PlainText is not appropriate for a real-world application. I will be updating this to have actual prod settings in the future.
+
 ## How to run
 
 `cd` into the Books.AppHost directory and then run the following command. (.Net 8 CLI is required to run this. )
