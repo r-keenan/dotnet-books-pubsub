f8fdfdf2abf55df2ca9bd8db775f59984941524f
r-keenan <ross.keenan@protonmail.com>
2024-11-18 09:50:43 -0600

    changed kafka message type from json to Avro for better performance while the message still being readable.

--- Books.API/Services/KafkaProducerConfig.cs
+++ Books.API/Services/KafkaProducerConfig.cs
@@ -4,4 +4,5 @@ public class KafkaProducerConfig
 {
     public string BootstrapServers { get; set; } = string.Empty;
     public string ClientId { get; set; } = string.Empty;
+    public string SchemaRegistryUrl { get; set; } = string.Empty;
 }
--- Books.API/Services/KafkaProducerService.cs
+++ Books.API/Services/KafkaProducerService.cs
@@ -1,26 +1,42 @@
-﻿using Books.API.Services;
+﻿using System.Text.Json;
+using Avro.Generic;
+using Books.API.Services;
 using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
 using Microsoft.Extensions.Options;
-using System.Text.Json;
 
 namespace Books.API;
 
-public class KafkaProducerService: IKafkaProducerService, IDisposable
+public class KafkaProducerService : IKafkaProducerService, IDisposable
 {
-    private readonly IProducer<string, string> _producer;
+    private readonly IProducer<string, GenericRecord> _producer;
+    private readonly ISchemaRegistryClient _schemaRegistry;
     private readonly ILogger<KafkaProducerService> _logger;
 
-    public KafkaProducerService(IOptions<KafkaProducerConfig> config, ILogger<KafkaProducerService> logger)
+    public KafkaProducerService(
+        IOptions<KafkaProducerConfig> config,
+        ILogger<KafkaProducerService> logger
+    )
     {
-        if (config == null) throw new ArgumentNullException(nameof(config));
+        if (config == null)
+            throw new ArgumentNullException(nameof(config));
 
         var producerConfig = new ProducerConfig()
         {
             BootstrapServers = config.Value.BootstrapServers,
-            ClientId = config.Value.ClientId
+            ClientId = config.Value.ClientId,
         };
 
-        _producer = new ProducerBuilder<string, string>(producerConfig).Build();
+        var schemaRegistryConfig = new SchemaRegistryConfig
+        {
+            Url = config.Value.SchemaRegistryUrl,
+        };
+
+        _schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        _producer = new ProducerBuilder<string, GenericRecord>(producerConfig)
+            .SetValueSerializer(new AvroSerializer<GenericRecord>(_schemaRegistry))
+            .Build();
         _logger = logger;
     }
 
@@ -28,17 +44,28 @@ public class KafkaProducerService: IKafkaProducerService, IDisposable
     {
         try
         {
-            // TODO: Serialize this as Avro instead of JSON for better performance
-            var jsonMessage = JsonSerializer.Serialize(message);
-            var kafkaMessage = new Message<string, string>
+            // Convert message to a GenericRecord using Avro schema
+            var schema = await _schemaRegistry.GetLatestSchemaAsync($"{topic}-value");
+            var avroSchema = (Avro.RecordSchema)Avro.Schema.Parse(schema.SchemaString);
+            var genericRecord = new GenericRecord(avroSchema);
+            var jsonString = JsonSerializer.Serialize(message);
+            var avroObj = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString);
+
+            foreach (var prop in avroObj)
+            {
+                genericRecord.Add(prop.Key, prop.Value);
+            }
+
+            var kafkaMessage = new Message<string, GenericRecord>
             {
                 Key = Guid.NewGuid().ToString(),
-                Value = jsonMessage
+                Value = genericRecord,
             };
 
             var deliveryResult = await _producer.ProduceAsync(topic, kafkaMessage);
-            _logger.LogInformation($"Message deliver to topic {topic} at partition {deliveryResult.Partition} with offset {deliveryResult.Offset}");
-
+            _logger.LogInformation(
+                $"Message deliver to topic {topic} at partition {deliveryResult.Partition} with offset {deliveryResult.Offset}"
+            );
         }
         catch (Exception e)
         {
