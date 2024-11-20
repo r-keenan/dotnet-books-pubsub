e2f2f82baed16e9d9fd359ec404bd391c8eb0e48
r-keenan <ross.keenan@protonmail.com>
2024-11-20 08:59:08 -0600

    Added logic for printing messages to the console for kafka messages coming from all three topics.

--- Books.Kafka.CreatedAuthorConsumer/Program.cs
+++ Books.Kafka.CreatedAuthorConsumer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Books.Kafka.Common;
+using Books.Shared.Constants;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+WriteLine("starting consumer...");
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(
+        (hostContext, config) =>
+        {
+            config
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    )
+    .ConfigureServices(
+        (context, services) =>
+        {
+            var kafkaConfig = context.Configuration.GetSection("Kafka").Get<KafkaConsumerConfig>();
+            if (kafkaConfig == null)
+            {
+                throw new ArgumentNullException(nameof(kafkaConfig));
+            }
+
+            services.Configure<KafkaConsumerConfig>(context.Configuration.GetSection("Kafka"));
+            services.AddSingleton<KafkaConsumer>(sp => new KafkaConsumer(
+                sp.GetRequiredService<IOptions<KafkaConsumerConfig>>(),
+                KafkaTopics.AuthorsTopic
+            ));
+
+            services.AddHostedService<KafkaConsumerHostedService>();
+        }
+    )
+    .Build()
+    .RunAsync();
--- Books.Kafka.CreatedBookConsumer/Program.cs
+++ Books.Kafka.CreatedBookConsumer/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello World");
+﻿using Books.Kafka.Common;
+using Books.Shared.Constants;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+WriteLine("starting consumer...");
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(
+        (hostContext, config) =>
+        {
+            config
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    )
+    .ConfigureServices(
+        (context, services) =>
+        {
+            var kafkaConfig = context.Configuration.GetSection("Kafka").Get<KafkaConsumerConfig>();
+            if (kafkaConfig == null)
+            {
+                throw new ArgumentNullException(nameof(kafkaConfig));
+            }
+
+            services.Configure<KafkaConsumerConfig>(context.Configuration.GetSection("Kafka"));
+            services.AddSingleton<KafkaConsumer>(sp => new KafkaConsumer(
+                sp.GetRequiredService<IOptions<KafkaConsumerConfig>>(),
+                KafkaTopics.BooksTopic
+            ));
+
+            services.AddHostedService<KafkaConsumerHostedService>();
+        }
+    )
+    .Build()
+    .RunAsync();
